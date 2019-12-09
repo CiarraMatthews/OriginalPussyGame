eccb6105c1e5c1a3505d089580c761bb630f2c50
Ciarra Matthews <50272922+CiarraMatthews@users.noreply.github.com>
2019-12-09 19:05:37 +0000

    Add files via upload

---
+++ texturescript.py
@@ -0,0 +1,32 @@
+import pygame
+
+pygame.init()
+
+bg = pygame.image.load("mainmenu.png")
+
+cat = pygame.image.load("catsprite1.png")
+
+#shooter = pygame.image.load("shooter.jpg")
+
+#BUTTONS
+
+start_button2 = pygame.image.load("start.png")
+start_button = pygame.image.load("sbutton.png")
+quit_button = pygame.image.load("quitter.png")
+quit_button2 = pygame.image.load("qbuttons.png")
+attackb = pygame.image.load("abutton.png")
+items = pygame.image.load("itemsb.png")
+taunt = pygame.image.load("tauntb.png")
+mapplz = pygame.image.load("mapplz3.png")
+punch1 = pygame.image.load("p_button.png")
+kick = pygame.image.load("k_button.png")
+cf2 = pygame.image.load("cf2.png")
+cf3 = pygame.image.load("cf3.png")
+cf4 = pygame.image.load("cf4.png")
+cf5 = pygame.image.load("cf5.png")
+cf6 = pygame.image.load("cf6.png")
+cf7 = pygame.image.load("cf7.png")
+cf8 = pygame.image.load("cf8.png")
+dudebroskyguy = pygame.image.load("dudebroskyguy.png")
+punchem = [cf2, cf3, cf4, cf5, cf6, cf7, cf8]
+hit = pygame.image.load("spritesheet.png")
